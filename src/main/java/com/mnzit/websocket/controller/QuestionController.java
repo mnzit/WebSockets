44796b8b180b04848ba76935c321b81225c1b340
Zuro Shakya <mnzitshakya@gmail.com>
2019-07-12 19:51:12 +0545

    Application based destination

---
+++ src/main/java/com/mnzit/websocket/controller/QuestionController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mnzit.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Mnzit
+ */
+@Controller
+public class QuestionController {
+
+    @MessageMapping("/questions")
+    public String processQuestion(String question) {
+        return question.toUpperCase();
+    }
+}
