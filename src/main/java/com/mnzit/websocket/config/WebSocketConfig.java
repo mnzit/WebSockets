b41009a226dc01a70160f99411eccd3ba5613445
Zuro Shakya <mnzitshakya@gmail.com>
2019-07-12 19:21:54 +0545

    Sock JS heart beat initiliazed

--- src/main/java/com/mnzit/websocket/config/WebSocketConfig.java
+++ src/main/java/com/mnzit/websocket/config/WebSocketConfig.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mnzit.config;
+package com.mnzit.websocket.config;
 
-import com.mnzit.handler.QuestionHandler;
+import com.mnzit.websocket.handler.QuestionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new QuestionHandler(), "/questions");
+        registry.addHandler(new QuestionHandler(), "/questions").withSockJS();
     }
     
 }
--- src/main/java/com/mnzit/websocket/handler/QuestionHandler.java
+++ src/main/java/com/mnzit/websocket/handler/QuestionHandler.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mnzit.handler;
+package com.mnzit.websocket.handler;
 
 import java.io.IOException;
 import java.util.List;
