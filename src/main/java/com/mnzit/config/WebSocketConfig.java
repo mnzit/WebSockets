29b04e95557d5422084df32d5cf41b79bef01fba
Zuro Shakya <mnzitshakya@gmail.com>
2019-07-12 19:03:45 +0545

    Using WebSocket Without Stomp

---
+++ src/main/java/com/mnzit/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mnzit.config;
+
+import com.mnzit.handler.QuestionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ *
+ * @author Mnzit
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new QuestionHandler(), "/questions");
+    }
+    
+}
